7d189efb261f20f9ba0a28132667f1701f022209
chendi <cdsama@gmail.com>
2016-08-30 21:51:34 +0800

    code reflector

--- LuaAutoBinding/Source/CodeGenerator.cpp
+++ LuaAutoBinding/Source/CodeGenerator.cpp
@@ -4,8 +4,10 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <list>
 #include <unordered_set>
 #include <unordered_map>
+#include <memory> 
 
 #include <rapidjson/rapidjson.h>
 #include <rapidjson/document.h>
@@ -17,7 +19,60 @@ struct CodeGenerator::Impl
     std::stringstream ssInclude;
     std::stringstream ssNormal;
     std::stringstream ssGlobal;
+
+    std::vector<std::shared_ptr<HeaderFile>> Files;
+
+    std::string CurrentFile;
+
     Document document;
+
+    struct Node {
+        std::string Name;
+    };
+
+    enum class FunctionType {
+        Common,
+        Static,
+        Global
+    };
+
+    struct LuaFunction : Node {
+        std::string Comment;
+        FunctionType Type;
+    };
+
+    struct LuaProperty : Node {
+        std::string Comment;
+        bool Writeable;
+    };
+
+    struct LuaEnum : Node {
+        std::list<std::string> Keys;
+        std::string Comment;
+    };
+
+    struct LuaClass : Node {
+        std::string ParentClass;
+        std::list<std::shared_ptr<LuaFunction>> Functions;
+        std::list<std::shared_ptr<LuaProperty>> Properties;
+        std::list<std::shared_ptr<LuaEnum>> Enums;
+        std::string Comment;
+    };
+
+    struct CxxNamespace : Node {
+        std::list<std::shared_ptr<LuaFunction>> StaticFunctions;
+        std::list<std::shared_ptr<CxxNamespace>> Namespaces;
+        std::list<std::shared_ptr<LuaClass>> Classes;
+        std::list<std::shared_ptr<LuaEnum>> Enums;
+    };
+
+    struct HeaderFile : Node {
+        std::list<std::string> IncludeFiles;
+        std::list<std::shared_ptr<LuaFunction>> GlobalFunctions;
+        std::list<std::shared_ptr<CxxNamespace>> Namespaces;
+        std::list<std::shared_ptr<LuaClass>> Classes;
+        std::list<std::shared_ptr<LuaEnum>> Enums;
+    };
 
     Impl()
     {
@@ -35,14 +90,14 @@ struct CodeGenerator::Impl
         buffer << ifs.rdbuf();
         ifs.close();
         document.Parse(buffer.str().c_str());
-        
+
         try
         {
             ParseDocument(document);
         }
         catch (std::string e)
         {
-            std::cerr << "Error: " << e << std::endl;
+            std::cerr << "Error: " << e << "Current File :" << CurrentFile << std::endl;
             return false;
         }
         return true;
@@ -50,76 +105,109 @@ struct CodeGenerator::Impl
 
     void ParseDocument(const Document& document)
     {
-        ssInclude << "#include <luaportal/LuaPortal.h>\n";
-        for (SizeType i = 0; i< document.Size(); ++i)
+        for (SizeType i = 0; i < document.Size(); ++i)
         {
             auto& v = document[i];
-            ParseFile(v);
+            Files.push_back(ParseFile(v));
         }
     }
 
-    void ParseFile(const Document::ValueType& Object)
+    std::shared_ptr<HeaderFile> ParseFile(const Document::ValueType& FileObject)
     {
-        ssInclude << "#include \"" << Object["file"].GetString() << "\"" << std::endl;
-        ParseContent(Object["content"]);
-    }
-
-    void ParseContent(const Document::ValueType& Value)
-    {
-        std::vector<std::string> NameSpaceStack;
-        for (SizeType i = 0; i< Value.Size(); ++i)
+        std::shared_ptr<HeaderFile> File = std::make_shared<HeaderFile>();
+        File->Name = FileObject["content"]->GetString();
+        CurrentFile = File->Name;
+        auto& ContentArray = FileObject["content"];
+        for (SizeType i = 0; i < ContentArray.Size(); ++i)
         {
-            auto& Unit = Value[i];
+            auto& Unit = ContentArray[i];
             std::string type = Unit["type"].GetString();
             if (type == "class")
             {
-                ParseClass(Unit, NameSpaceStack);
+                File->Classes.push_back(ParseClass(Unit));
+            }
+            else if (type == "function")
+            {
+                File->GlobalFunctions.push_back(ParseFunction(Unit));
+            }
+            else if (type == "include")
+            {
+                File->IncludeFiles.push_back(Unit["file"].GetString());
+            }
+            else if (type == "enum")
+            {
+                File->Enums.push_back(ParseEnum(Unit));
             }
         }
+        return File;
     }
 
-    std::string GetNameSpaceHead(const std::vector<std::string>& NameSpaceStack)
+
+    std::shared_ptr<LuaEnum> ParseEnum(const Document::ValueType&& EnumObject)
     {
-        std::stringstream ss;
-        for (auto& ns : NameSpaceStack)
-        {
-            ss << ns << "::";
-        }
-        return ss.str();
+        std::shared_ptr<LuaEnum> Enum = std::make_shared<LuaEnum>();
+        Enum->Name = EnumObject["name"];
+        return Enum;
     }
 
-    void ParseClass(const Document::ValueType& ClassObject, std::vector<std::string>& NameSpaceStack)
+    std::shared_ptr<LuaClass> ParseClass(const Document::ValueType& ClassObject)
     {
-        std::string ClassName = ClassObject["name"].GetString();
-        std::string ParentClassName;
-        auto NameSpaceHead = GetNameSpaceHead(NameSpaceStack);
-        if (!ClassObject["meta"].HasMember("noinherit") && GetClassParent(ClassObject, ParentClassName))
+        std::shared_ptr<LuaClass> Class = std::make_shared<LuaClass>();
+        Class->Name = ClassObject["name"].GetString();
+        if (!ClassObject["meta"].HasMember("noinherit"))
         {
-            
-            if (RegistedClasses.find(ParentClassName) != RegistedClasses.end())
-            {
-                ssNormal << ".DeriveClass<" << NameSpaceHead << ClassName << ", " << ParentClassName << ">(\"" << ClassName << "\")" << std::endl;
-            }
-            else
-            {
-
-            }
-        }
-        else 
-        {
-            ssNormal << ".BeginClass<" << NameSpaceHead << ClassName << ">(\"" << ClassName << "\")" << std::endl;
+            GetClassParent(ClassObject, Class->ParentClass);
         }
 
-        NameSpaceStack.push_back(ClassName);
+        auto& ClassMembers = ClassObject["members"];
+        for (SizeType i = 0; i < ClassMembers.Size(); ++i)
+        {
+            auto& Unit = ClassMembers[i];
+            std::string type = Unit["type"].GetString();
+            if (type == "function")
+            {
+                Class->Functions.push_back(ParseFunction(Unit));
+            }
+        }
 
-        NameSpaceStack.pop_back();
+        return Class;
+    }
 
-        ssNormal << ".EndClass()" << std::endl;
+    std::shared_ptr<LuaFunction> ParseFunction(const Document::ValueType& FunctionObject)
+    {
+        std::shared_ptr<LuaFunction> Function = std::make_shared<LuaFunction>();
+        Function->Name = FunctionObject["name"];
+        Function->Type = FunctionType::Common;
+        auto itr = FunctionObject.FindMember("access");
+        if (itr != FunctionObject.End())
+        {
+            if (itr->value.GetString() != "public")
+            {
+                stringstream ss;
+                ss << "Function :" << Function->Name << "must be public, Line: " << FunctionObject["line"]->GetInt();
+                throw ss.str();
+            }
+        }
+        bool IsStatic = false;
+        itr = FunctionObject.FindMember("static");
+        if (itr != FunctionObject.End())
+        {
+            IsStatic = itr->value.GetBool();
+        }
+
+        if (IsStatic)
+        {
+            auto& FunctionMeta = FunctionObject["meta"];
+            Function->Type = (FunctionMeta->HasMember("global"))
+                ? FunctionType::Global
+                : FunctionType::Static;
+        }
+        return Function;
     }
 
     bool GetClassParent(const Document::ValueType& ClassObject, std::string& OutParentClassName)
     {
-        auto &ParentsArray= ClassObject["parents"];
+        auto &ParentsArray = ClassObject["parents"];
         if (!ParentsArray.IsArray())
         {
             return false;
@@ -152,6 +240,8 @@ struct CodeGenerator::Impl
             << "\t;\n}" << std::endl;
         return ss.str();
     }
+
+
 
 };
 
