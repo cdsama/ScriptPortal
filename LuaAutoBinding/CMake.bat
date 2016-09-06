cfc144dc7a5cbb364035bb466972b6e1b6a11ebb
chendi <cdsama@gmail.com>
2016-09-06 11:30:10 +0800

    add build script

---
+++ LuaAutoBinding/CMake.bat
@@ -0,0 +1,21 @@
+@echo off
+cd /d "%~dp0"
+
+REM get visual studio installation directory from registry
+for /f "tokens=1,2*" %%a in ('reg query "HKEY_CURRENT_USER\SOFTWARE\Microsoft\VisualStudio\14.0_Config" /v "InstallDir" ^| findstr "InstallDir"') do (
+set VS_HOME=%%c
+)
+
+if not exist Build (
+    md Build
+)
+
+cd Build
+
+cmake -G "Visual Studio 14 2015 Win64" ..
+
+echo %VS_HOME%
+
+"%VS_HOME%devenv.com" "LuaAutoBinding.sln" /project "INSTALL" /rebuild "Release|x64"
+
+pause
